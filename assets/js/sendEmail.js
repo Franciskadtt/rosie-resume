68790564995e565e114e64538cf598d24a741af0
Franciskadtt <franciskadtt@gmail.com>
2021-02-18 15:02:51 +0000

    Implement emailjs integration on the contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("service_kl3smq8","rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+}
